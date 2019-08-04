c764cf92fbe0c6e6277c2fa36d81e8943c7ce2ff
miyamoen <akihito.biblioteca@gmail.com>
2019-08-04 21:55:26 +0900

    Hello, Ayame!

--- src/CameraVideo.js
+++ src/CameraVideo.js
@@ -1,35 +1,47 @@
+import { connection as AyameConnection } from "@open-ayame/ayame-web-sdk";
+
 class CameraVideo extends HTMLElement {
   constructor() {
     super();
   }
   connectedCallback() {
-    const video = document.createElement("video");
-    video.playsinline = true;
-    video.muted = true;
-    video.mute = true;
-    video.autoplay = true;
-    video.poster = "/img/camera_play.svg";
-    video.style.width = "100%";
+    const localVideo = createVideo();
+    const remoteVideo = createVideo();
 
-    video.addEventListener("click", e => {
-      console.log("click", e);
-      if (this.mediaStream && this.mediaStream.active) {
-        this.mediaStream.getTracks().forEach(track => track.stop());
-        video.srcObject = null;
+    this.addEventListener("click", e => {
+      if (this.conn && this.conn.stream.active) {
+        this.conn.stream.getTracks().forEach(track => track.stop());
+        this.conn._ws.close();
+        localVideo.srcObject = null;
+        this.conn = null;
       } else {
-        navigator.mediaDevices
-          .getUserMedia({ audio: false, video: true })
-          .then(mediaStream => {
-            window.ms = mediaStream;
-            this.mediaStream = mediaStream;
-            video.srcObject = mediaStream;
-          })
-          .catch(err => console.log(err));
+        this.conn = AyameConnection(
+          "wss://ayame.shiguredo.jp/ws",
+          "miyamo-test-room",
+        );
+        const startConn = async () => {
+          const mediaStream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: true,
+          });
+          const stream = await this.conn.connect(mediaStream);
+          this.conn.on("disconnect", e => {
+            console.log(e);
+            remoteVideo.srcObject = null;
+          });
+          this.conn.on("addstream", e => {
+            remoteVideo.srcObject = e.stream;
+          });
+          localVideo.srcObject = stream;
+        };
+        startConn();
       }
     });
 
-    this.video = video;
-    this.appendChild(video);
+    this.localVideo = localVideo;
+    this.remoteVideo = remoteVideo;
+    this.appendChild(localVideo);
+    this.appendChild(remoteVideo);
   }
 
   disconnectedCallback() {}
@@ -38,6 +50,17 @@ class CameraVideo extends HTMLElement {
     return [];
   }
   attributeChangedCallback(name, oldValue, newValue) {}
+}
+
+function createVideo() {
+  const video = document.createElement("video");
+  video.playsinline = true;
+  video.muted = true;
+  video.mute = true;
+  video.autoplay = true;
+  video.poster = "/img/camera_play.svg";
+  video.style.width = "40%";
+  return video;
 }
 
 export default {
